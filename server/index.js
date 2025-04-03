38a9bb967df2bc29c732af9f29a357c5437bb0f0
Palinda <palinda009@outlook.com>
2025-04-03 11:38:05 +0530

    fourth commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
 const app = express();
 const PORT = 5000;
 
@@ -8,14 +11,12 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/schoolDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB Connected"))
+// MongoDB Connection
+mongoose.connect('mongodb+srv://apuathuckorala:palinda2001@cluster0.sqgqjor.mongodb.net/schoolDB?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.error(err));
 
-// Student Schema
+// Mongoose Schema
 const Student = mongoose.model('Student', {
   name: String,
   email: String,
@@ -24,15 +25,93 @@ const Student = mongoose.model('Student', {
   lectureMode: String   // Online or Onsite
 });
 
-// Routes
+// Swagger Setup
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Student API',
+      version: '1.0.0',
+      description: 'API for managing student records',
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+      },
+    ],
+  },
+  apis: ['./index.js'], // Path to the file with Swagger comments
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Student:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *         - course
+ *         - deliveryMode
+ *         - lectureMode
+ *       properties:
+ *         name:
+ *           type: string
+ *         email:
+ *           type: string
+ *         course:
+ *           type: string
+ *         deliveryMode:
+ *           type: string
+ *         lectureMode:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * /students:
+ *   get:
+ *     summary: Get all students
+ *     tags: [Student]
+ *     responses:
+ *       200:
+ *         description: List of students
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Student'
+ */
 app.get('/students', async (req, res) => {
   const students = await Student.find();
   res.json(students);
 });
 
+/**
+ * @swagger
+ * /students:
+ *   post:
+ *     summary: Create a new student
+ *     tags: [Student]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Student'
+ *     responses:
+ *       200:
+ *         description: Student created successfully
+ */
 app.post('/students', async (req, res) => {
   const student = await Student.create(req.body);
   res.json(student);
 });
 
+// Start Server
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
